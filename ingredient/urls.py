2446a7bbff2ddf0aa4faced780beb0ab938d05fc
cahyacoding <cahya.bgg@gmail.com>
2024-10-27 21:27:49 +0700

    merubah url ingredient membuang yang tidak berguna

--- ingredient/urls.py
+++ ingredient/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from ingredient.views import (
-    show_filter,record_ingredients,show_card,search_ingredient
+    show_filter,record_ingredients,search_ingredient
     )
 
 
@@ -9,6 +9,5 @@ app_name = 'ingredient'
 urlpatterns = [
     path('', show_filter, name='show_filter'),
     path('record_ingredients/', record_ingredients, name='record_ingredients'),
-    path("product_card/",show_card),
     path("search_ingredient/",search_ingredient,name= "search_ingredient")
 ]
